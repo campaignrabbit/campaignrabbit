c61cb07c92e68d3b0bd0668c1407fa2bd3719123
Alagesan <alagesan@flycart.org>
2018-12-06 12:52:43 +0530

    - customer synchronize either via order info or user address

--- system_campaignrabbit/campaignrabbit.php
+++ system_campaignrabbit/campaignrabbit.php
@@ -64,7 +64,15 @@ class plgSystemCampaignrabbit extends JPlugin {
             $document = JFactory::getDocument();
             $params = $this->getPluginParams();
             $app_id = $params->get('app_id', '');
+            $analaysis_array = array(
+                'app_id' => $app_id
+            );
+            $user = JFactory::getUser();
+            if($user->id > 0){
+                $analaysis_array['email'] = $user->email;
+            }
             $script_content = 'window.app_id = "'.$app_id.'";
+            window.campaignrabbit = '.json_encode($analaysis_array).';
                 !function(e,t,n,p,o,a,i,s,c){e[o]||(i=e[o]=function(){i.process?i.process.apply(i,arguments):i.queue.push(arguments)},i.queue=[],i.t=1*new Date,s=t.createElement(n),s.async=1,s.src=p+"?t="+Math.ceil(new Date/a)*a,c=t.getElementsByTagName(n)[0],c.parentNode.insertBefore(s,c))}(window,document,"script","https://cdn.campaignrabbit.com/campaignrabbit.analytics.js","rabbit",1),rabbit("init",window.app_id),rabbit("event","pageload");';
             $document->addScriptDeclaration($script_content);
         }
